3446a251b789ba0f7d21f4c373e79c37713c7f02
takkasila <takkasila@outlook.com>
2014-09-21 15:40:57 +0700

    Successfully import Slick2D in Java Build Path and beautifuly extends the class from BasicGame. And not to mentioned that also setting variables up and created a gougeous renderable resault mighty howling black screen. <3

--- src/RelativeGame.java
+++ src/RelativeGame.java
@@ -1,3 +1,4 @@
+import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
@@ -5,14 +6,22 @@ import org.newdawn.slick.SlickException;
 
 
 public class RelativeGame extends BasicGame {
+	
+	final static int FRAME_PER_SECOND = 60;
+	final static int SCREEN_WIDTH = 800;
+	final static int SCREEN_HEIGHT = 600;
 
 	public RelativeGame(String title) {
 		super(title);
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws SlickException {
+		RelativeGame game = new RelativeGame("Relative Game");
+		AppGameContainer appgc = new AppGameContainer(game, 0, 0, false);
+		appgc.setDisplayMode(SCREEN_WIDTH, SCREEN_HEIGHT, false);
+		appgc.setTargetFrameRate(FRAME_PER_SECOND);
+		appgc.start();
 
 	}
 
