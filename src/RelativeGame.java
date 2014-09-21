ae158ff70551b743d0f4d6d507c7d3575290891d
takkasila <takkasila@outlook.com>
2014-09-22 01:38:23 +0700

    Added player.

--- src/RelativeGame.java
+++ src/RelativeGame.java
@@ -2,6 +2,7 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 
@@ -10,10 +11,11 @@ public class RelativeGame extends BasicGame {
 	final static int FRAME_PER_SECOND = 60;
 	final static int SCREEN_WIDTH = 800;
 	final static int SCREEN_HEIGHT = 600;
+	
+	static Player player;
 
 	public RelativeGame(String title) {
 		super(title);
-		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) throws SlickException {
@@ -26,21 +28,37 @@ public class RelativeGame extends BasicGame {
 	}
 
 	@Override
-	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public void init(GameContainer arg0) throws SlickException {
-		// TODO Auto-generated method stub
-		
+		player = new Player(100, 100, "SAMPLE");
+	}
+	
+	@Override
+	public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+		player.Render(gameContainer, graphics);
 	}
 
+
 	@Override
-	public void update(GameContainer arg0, int arg1) throws SlickException {
-		// TODO Auto-generated method stub
-		
+	public void update(GameContainer gameContainer, int delta) throws SlickException {
+		player.Update(gameContainer, delta);
+	}
+	
+	
+	@Override
+	public void keyPressed(int key, char c)
+	{
+		if(key == Input.KEY_SPACE)
+		{
+			player.Jump();
+		}
+		if(key == Input.KEY_RIGHT)
+		{
+			player.Walk_Right();
+		}
+		if(key == Input.KEY_LEFT)
+		{
+			player.Walk_Left();
+		}
 	}
 
 }
