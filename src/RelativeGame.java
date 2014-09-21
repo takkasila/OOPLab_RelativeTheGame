4c68c2d4b4918ff259d9132866408f7686ae0522
takkasila <takkasila@outlook.com>
2014-09-21 14:22:55 +0700

    Created project

---
+++ src/RelativeGame.java
@@ -0,0 +1,37 @@
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+
+public class RelativeGame extends BasicGame {
+
+	public RelativeGame(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
